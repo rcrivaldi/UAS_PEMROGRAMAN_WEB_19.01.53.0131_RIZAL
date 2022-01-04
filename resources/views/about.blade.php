51ed84b25b58e21607587b90918fc5d20af8deb9
rcrivaldi <97105017+rcrivaldi@users.noreply.github.com>
2022-01-05 03:10:22 +0700

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Unisbank</title>
+</head>
+<body>
+    <a href="home">Home</a>
+    <a href="profile">Profile</a>
+    <h1>Universitas Stikubank</h1>
+    <h2>Digital Youth</h2>
+    <h2>Enterpreneurial University</h2>
+    <h3>#KampusMerdeka</h3>
+    <h3>#KampusWirausaha</h3>
+    <h3>#KampusAnakMuda</h3>
+</body>
+</html>
