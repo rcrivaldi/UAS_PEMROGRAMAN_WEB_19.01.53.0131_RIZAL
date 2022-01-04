3b0db835ea0f89d0ebf845235083b075ac7e822d
rcrivaldi <97105017+rcrivaldi@users.noreply.github.com>
2022-01-05 03:11:20 +0700

    Add files via upload

---
+++ storage/framework/views/0ac484d49ab02dd0aab9316f526166aee3918632.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>19.01.53.0131 Rizal</title>
+</head>
+<body>
+    <a href="profile">Profile</a>
+    <a href="about">Unisbank</a>
+    <h1>Ujian Akhir Semester Pemrograman Web</h1>
+    <h2>Tutorial Instalasi Laravel di Windows 10 via Composer :</h2>
+    <h3>1. Install PHP dan Composer terlebih dahulu pada PC Anda</h3>
+    <h3>2. Buat folder baru pada directory Anda untuk menyimpan aplikasi Laravel</h3>
+    <h3>3. Buka terminal PC Anda, lalu masuk pada directory yang sudah Anda buat sebelumnya</h3>
+    <h3>4. Lanjut tuliskan perintah 'composer create-project laravel/laravel example-app' pada terminal PC Anda untuk memulai download Laravel</h3>
+    <h3>5. Tunggu hingga proses selesai, dan pastikan koneksi internet pada PC Anda stabil</h3>
+    <h3>6. Apabila sudah muncul keterangan 'Application key set successfully.' selamat artinya Laravel sudah selesai di download</h3>
+</body>
+</html><?php /**PATH D:\applications\coba-laravel\resources\views/home.blade.php ENDPATH**/ ?>
